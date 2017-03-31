78cef5eb483144111b4ca6e4d0f48c729bfa5698
xu.minzhe@haixiuyizhan.com <xu.minzhe@haixiuyizhan.com>
2017-03-31 18:32:17 +0800

    注解

--- SpringShiro/src/main/java/demo/realm/MyRealm.java
+++ SpringShiro/src/main/java/demo/realm/MyRealm.java
@@ -12,7 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import demo.entity.User;
 import demo.service.UserService;
-
+/**
+ * 
+ * @author Minzhe Xu	2017年3月31日下午5:54:49
+ *shiro提供了抽象类 AuthenticatingRealm 专门用于从Realm中获得认证信息。
+ *所以我们可以继承 抽象类 AuthenticatingRealm，然后实现其中的抽象方法
+ *我们也可以使用 AuthenticatingRealm 的子类 AuthorizingRealm，
+ *它本来是用于权限认证的Realm，但是因为他继承了 AuthenticatingRealm，
+ *所以实际上我们只要继承 AuthorizingRealm，然后实现它的抽象方法就行了。
+ *同时搞定 登录认证 和 权限认证(访问控制)
+ */
 public class MyRealm extends AuthorizingRealm {
 	
 	@Autowired
